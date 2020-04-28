59c6c3f5bf82c12f2290af9731b83e83507203bf
Andres Condo <1dkevinandres.b@gmail.com>
2020-04-28 13:12:09 -0500

    ingreso de datos

--- arithmetic_progression.py
+++ arithmetic_progression.py
@@ -1,3 +1,24 @@
 #-*- coding:utf-8 -*-
 
 #Calculadora de progresiones aritmeticas
+
+def run():
+    a1 = int(raw_input('Ingrese el primer término: '))
+    a2 = int(raw_input('Ingrese el segundo término: '))
+    
+    dife = str(raw_input('''
+        Se le dío el valor de la diferencia?
+        [s]i
+        [n]o
+        '''))
+    if dife =='s':
+        d = int(raw_input('Ingrese la diferencia: '))
+
+
+
+
+
+
+if __name__ == '__main__':
+    print('INGRESE LOS DATOS DE LA PROGRESIÓN ARITMÉTICA')
+    run()
