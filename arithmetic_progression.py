e785d54ba7bbf848c33797ea0d90c192d9f4b66b
Andres Condo <1dkevinandres.b@gmail.com>
2020-04-28 13:37:45 -0500

    añadido la funcion para las operaciones con diferencia(solo generado relacion entre ellos)

--- arithmetic_progression.py
+++ arithmetic_progression.py
@@ -2,18 +2,34 @@
 
 #Calculadora de progresiones aritmeticas
 
-def run():
-    a1 = int(raw_input('Ingrese el primer término: '))
-    a2 = int(raw_input('Ingrese el segundo término: '))
-    
-    dife = str(raw_input('''
-        Se le dío el valor de la diferencia?
-        [s]i
-        [n]o
-        '''))
-    if dife =='s':
-        d = int(raw_input('Ingrese la diferencia: '))
+def with_difference(primer, segundo, d):
+    result = 0
 
+    result = float((primer + segundo) / d)
+
+    return result
+
+
+def run():
+    primer = int(raw_input('Ingrese el primer término: '))
+    segundo = int(raw_input('Ingrese el segundo término: '))
+    
+    while True:
+        dife = str(raw_input('''
+            Se le dío el valor de la diferencia?
+            [s]i
+            [n]o
+            '''))
+        if dife =='s':
+            d = int(raw_input('Ingrese la diferencia: '))
+            cal_with_diference = with_difference(primer, segundo, d)
+            print('El resultado es: {}'.format(cal_with_diference))
+
+        if dife == 'n':
+            no_difference(a1, a2)
+
+        else:
+            print('Ingrese la letra entre los corchetes')
 
 
 
